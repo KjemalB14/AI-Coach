fda67546833b831c590a9111ccdd799f46edd506
KjemalB14 <151947960+KjemalB14@users.noreply.github.com>
2025-06-24 20:07:01 -0400

    updated UI

--- ai-coach-app/src/app/app.ts
+++ ai-coach-app/src/app/app.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SafeUrl, DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
 export class App {
   protected title = 'ai-coach-app';
+  videoUrl: SafeUrl | null = null;
+
+  constructor(private sanitizer: DomSanitizer) {}
+
+  onFileSelected(event: Event): void {
+    const input = event.target as HTMLInputElement;
+
+    if (input.files && input.files[0]) {
+      const file = input.files[0];
+      const objectUrl = URL.createObjectURL(file);
+      this.videoUrl = this.sanitizer.bypassSecurityTrustUrl(objectUrl);
+    }
+  }
 }
